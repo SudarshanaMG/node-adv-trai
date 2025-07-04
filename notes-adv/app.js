63e17a0027b4a4cbb7d2d832551930d84bcc1aea
Sudarshana Melige Gurumurthy(UST <294238@ust.com>
2025-07-04 10:09:46 +0530

    add-yargs

--- notes-adv/app.js
+++ notes-adv/app.js
@@ -1,42 +1,94 @@
 const { addNote, removeNote, listNote } = require('./notes');
 const chalk = require('chalk');
-const args = process.argv.slice(2);
-const command = args[0];
 
-const getArgValue = (flag) => {
-    const index = args.indexOf(flag);
-    if(index !== -1 && args[index + 1]){
-        return args[index + 1].replace(/^["']|["']$/g, '');
-    }
-    return null;
-}
+const yargs = require('yargs')
+yargs.version('1.0.0')
 
-switch (command) {
-    case 'add': {
-        const title = getArgValue('--title');
-        const body = getArgValue('--body');
-        if(!title || !body){
-            console.log(chalk.yellow('Please provide --title and --body to add a note.'));
+// const args = process.argv.slice(2);
+// const command = args[0];
+
+// const getArgValue = (flag) => {
+//     const index = args.indexOf(flag);
+//     if(index !== -1 && args[index + 1]){
+//         return args[index + 1].replace(/^["']|["']$/g, '');
+//     }
+//     return null;
+// }
+
+// switch (command) {
+//     case 'add': {
+//         const title = getArgValue('--title');
+//         const body = getArgValue('--body');
+//         if(!title || !body){
+//             console.log(chalk.yellow('Please provide --title and --body to add a note.'));
+//         }
+//         else{
+//             addNote(title, body);
+//         }
+//         break;
+//     }
+//     case 'list': {
+//         listNote();
+//         break;
+//     }
+//     case 'remove': {
+//         const title = getArgValue('--title');
+//         if(!title){
+//             console.log(chalk.red('Please provide --title to remove a note.'));
+//         }
+//         else{
+//             removeNote(title);
+//         }
+//         break;
+//     }
+//     default: console.log(chalk.red('Unknown command. \nUse: add, list, remove'));
+//             console.log(chalk.red(`Examples: \nnode notes.js add --title "Meeting" --body "Client call at 4PM" \nnode notes.js list \nnode notes.js remove --title "Meeting"`));
+// }
+
+yargs.command({
+    command: "add",
+    description: "Add a new note",
+    builder: {
+        title: {
+            description: "Note title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            description: "Note body",
+            demandOption: true,
+            type: "string"
         }
-        else{
-            addNote(title, body);
-        }
-        break;
+    },
+    handler(argv) {
+        addNote(argv.title, argv.body);
     }
-    case 'list': {
+});
+
+yargs.command({
+    command: "list",
+    description: "List all notes",
+    handler() {
         listNote();
-        break;
     }
-    case 'remove': {
-        const title = getArgValue('--title');
-        if(!title){
-            console.log(chalk.red('Please provide --title to remove a note.'));
+});
+
+yargs.command({
+    command: "remove",
+    description: "Remove a note",
+    builder: {
+        title: {
+            description: "Note title to remove",
+            demandOption: true,
+            type: "string"
         }
-        else{
-            removeNote(title);
-        }
-        break;
+    },
+    handler(argv) {
+        removeNote(argv.title);
     }
-    default: console.log(chalk.red('Unknown command. \nUse: add, list, remove'));
-            console.log(chalk.red(`Examples: \nnode notes.js add --title "Meeting" --body "Client call at 4PM" \nnode notes.js list \nnode notes.js remove --title "Meeting"`));
+});
+
+if(process.argv.length <= 2){
+    yargs.showHelp();
 }
+yargs.parse();
